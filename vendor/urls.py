1897889e014b27ebc31fffa482ed4d37aec34818
Ramesh <101050352+RAmxsda@users.noreply.github.com>
2023-02-10 09:38:54 +0545

    added vendor food CURD

--- vendor/urls.py
+++ vendor/urls.py
@@ -10,9 +10,13 @@ urlpatterns = [
     path('menu-builder/',views.menu_builder,name='menu_builder'),
     path('menu-builder/category/<int:pk>/', views.fooditems_by_category, name='fooditems_by_category'),
 
-
+    #Menu CURD 
     path('menu-builder/category/add/', views.add_category, name='add_category'),
     path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
     path('menu-builder/category/delete/<int:pk>/', views.delete_category, name='delete_category'),
 
+    #Menu-food CURD
+    path('add-food/category/add/', views.add_food, name='add_food'),
+    path('add-food/category/edit/<int:pk>/', views.edit_food, name='edit_food'),
+    path('add-food/category/delete/<int:pk>/', views.delete_food, name='delete_food'),
 ]
