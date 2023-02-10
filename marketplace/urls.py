c55c6527d5883b86ada70df667ce40c3d689e048
Ramesh <101050352+RAmxsda@users.noreply.github.com>
2023-02-10 22:22:31 +0545

    implementing cart functionality makes my mind hangover

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -5,5 +5,14 @@ urlpatterns = [
     path('',views.marketplace, name='marketplace'),
     path('<slug:vendor_slug>/',views.vendor_detail, name='vendor_detail'),
     
+    #ADD to cart
+    path('add_to_cart/<int:food_id>/', views.add_to_cart, name='add_to_cart'),
+    # DECREASE CART
+    path('decrease_cart/<int:food_id>/', views.decrease_cart, name='decrease_cart'),
+    # DELETE CART ITEM
+    path('delete_cart/<int:cart_id>/', views.delete_cart, name='delete_cart'),
+    
+    # if we put the below path then it will give error thinnking that it is vendor_slug
+    # path('cart/',views.cart,name='cart'),
 
 ]
